301c4cf5ae74542a3a123a2c1c2de425e9a8401f
Sven Kleinert <kleinert@iqo.uni-hannover.de>
2019-03-24 22:06:08 +0100

    extended ptychographic iterative engine added

--- pyfrog/retrieval.py
+++ pyfrog/retrieval.py
@@ -284,3 +284,105 @@ def rana_spectrum( trace, padding=None, threshold=1e-3 ):
     spectrum = np.concatenate( ( spectrum[:int(Nf/2)], spectrum[-int(Nf/2):] ) )
     #TODO Why it doesn't require squaring here?
     return spectrum / np.amax( spectrum )
+
+
+def epie( trace,
+                   g_limit=1e-10,
+                   max_iter=500,
+                   max_time=10,
+                   stagnation_count = 100,
+                   show_every=None,
+                   spectrum=None, phase=None,
+                   spectrum_gate=None, phase_gate=None,
+                   nonlinearity = 'shg' ):
+    """
+        epie( trace, gamma, *kwargs ) retrieves the complex electric field of a pulse from a FROG trace using the extended ptychographical iterative engine (ePIE). The whole procedure is desribed in 'Pavel Sidorenko, Oren Lahav, Zohar Avnat, and Oren Cohen, "Ptychographic reconstruction algorithm for frequency-resolved optical gating: super-resolution and supreme robustness," Optica 3, 1320-1330 (2016)'.
+
+        arguments:
+            trace : FROG trace.
+            g_limit : default 1e-5, retrieval is stopped if the error is smaller than g_limit.
+            max_iter : default 1e3, maximum number of iterations allowed.
+            max_time : default 10, time in seconds allowed.
+            stagnation_count : default 100, allow the algorithm to continue for 'stagnation_count' iterations without improvement before it is stopped.
+            show_every : default 1e30, shows the FROG error every 'show_every' iterations.
+            spectrum : vector containing the power spectral density (PSD) of the pulse.
+            phase : vector containing the spectral phase of the pulse.
+            spectrum_gate (optional) : vector containing the PSD of the gate pulse.
+            phase_gate (optional) : vector containing the spectral phase of the gate pulse.
+            nonlinearity : name of the nonlinearity. Default is 'shg'. Other nonlinearities are currently not available
+
+        returns:
+            E : complex electric field of the pulse in spectral domain.
+                retrieved spectrum is therefore abs(E)**2
+                retrieved phase is np.angle(E)
+    """
+    Nf = trace.shape[1] 
+    if show_every is None:
+        show_every = 1e30
+    if spectrum is None:
+        spectrum = np.random.rand( Nf )
+    if phase is None:
+        phase = np.random.rand( Nf ) * 2 * np.pi
+    E_f = np.sqrt( abs(spectrum) ) * np.exp( -1j * phase )
+    E_t = np.fft.ifft( E_f )
+
+
+    if nonlinearity == 'shg':
+        _gamma = lambda x: x
+        _invgamma = lambda x: x
+    E_best = E_t.copy()
+    converged = False
+    G = 1e30
+    gbest = 1e30
+    not_improved = 0
+    iterations = 0
+    start = time.time()
+    if not 'errors' in globals():
+        global errors
+        errors = []
+    while iterations < max_iter and \
+            (time.time()-start) < max_time and \
+            G > g_limit and \
+            not_improved < stagnation_count:
+        iterations += 1
+        sim_trace, O_f = pyfrog.frog._generate_trace( E_t, E_t, return_field=True )
+        G = pyfrog.frog.g_error( trace, sim_trace )
+        errors.append( G )
+        if iterations%show_every == 0:
+            print( "ePIE, error={:.3e}, time elapsed={:.2f}s".format(gbest, time.time()-start) )
+        if gbest < G:
+            not_improved += 1
+        else:
+            not_improved = 0
+            gbest = G
+            E_best = E_t.copy()
+
+        alpha = np.random.uniform( 0.1, 0.5 )
+        J = np.random.permutation( Nf )
+        for j in J:
+            to_roll = j - int( Nf / 2 )
+            E_shifted = np.roll( E_t, to_roll )
+            psi_t = E_t * E_shifted
+            psi_w = np.fft.fft( psi_t )
+
+            # mute the warning created by devision of abs(O_f)
+            old_err_settings = np.seterr( invalid='ignore' )
+            psi_w = psi_w / abs( psi_w ) * np.sqrt( trace[:,j] )
+            np.seterr( invalid=old_err_settings['invalid'] )
+
+            psi_w[ np.isnan( psi_w ) ] = 0
+            psi_t_prime = np.fft.ifft( psi_w ) * Nf
+            
+            E_t = E_t + alpha * ( np.conjugate( E_shifted ) / np.amax( abs( E_t )**2 ) * (psi_t_prime - psi_t)
+                    + np.roll( np.conjugate( E_t ) / np.amax( abs( E_t )**2 ) * (psi_t_prime-psi_t), -to_roll ) )
+
+            #E_t = E_t + alpha * np.roll( np.conjugate( E_t ), to_roll ) /np.max( abs(E_t) )**2 * ( psi_t_prime - psi_t )
+            
+
+        E_t = E_t / np.amax(abs(E_t))
+
+    E_t = pyfrog.grid.remove_group_delay( E_best )
+    E = np.fft.ifft( E_t )
+    E /= np.amax( abs( E ) )
+    #TODO why is the frequency axis reversed
+    return np.flip( E )
